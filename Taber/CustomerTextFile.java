ae218f0a5eaf91e7ba4bae33cd65ba883aea8350
MorganSkye <46610639+MorganSkye@users.noreply.github.com>
2019-04-04 15:18:52 -0500

    Add files via upload

---
+++ Taber/CustomerTextFile.java
@@ -0,0 +1,82 @@
+package murach.io;
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import murach.business.Customer;
+
+public class CustomerTextFile {
+    private static final String FIELD_SEP = "\t";
+    private static final Path customersPath = null;      // add code here
+    private static final File customersFile = null;      // add code here
+    private static ArrayList<Customer> customers = null; // add code here
+    
+    // prevent instantiation of the class
+    private CustomerTextFile() {}
+    
+    	customerPath = get("Customertxt") {
+    		    
+    	}
+
+    public static List<Customer> getCustomers() {
+        // if the customers file has already been read, don't read it again
+    	if (customers != null) {
+	return customers;
+	}
+    	if (!Files.exists(customersPath)) {
+    		return null;
+    	}
+	 = new ArrayList<>();
+        
+        // load the array list with Customer objects created from
+        // the data in the file
+        
+
+        return customers;            
+    }
+
+    public static Customer getCustomer(String email) {
+        for (Customer c : customers) {
+            if (c.getEmail().equals(email)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public static boolean addCustomer(Customer c) {
+        customers.add(c);
+        return saveCustomers();
+    }
+
+    public static boolean deleteCustomer(Customer c) {
+        customers.remove(c);
+        return saveCustomers();
+    }
+
+    
+    public static Customer getCustomer(String email) {
+    	for (Customer c : customers) {
+    		if (c.getEmail().equals(email)) {
+    			return c;
+    
+    			public static boolean updateCustomer(Customer newCustomer) {
+        // get the old customer and remove it
+        Customer oldCustomer = getCustomer(newCustomer.getEmail());
+        int i = customers.indexOf(oldCustomer);
+        customers.remove(i);
+
+        // add the updated customer
+        customers.add(i, newCustomer);
+
+        return saveCustomers();
+    }
+
+    private static boolean saveCustomers() {
+        // save the Customer objects in the array list to the file
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(customersFile)));
+		
+    	
+    	return true;
+    }
+}
